cbc65c3c1bd71eb46096be401cb8ef39708761c8
all1nol <newacckim@gmail.com>
2025-12-24 11:27:55 +0400

    use sys arguments

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from stats import get_word_count, get_char_count, get_sorted_list
+import sys 
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1] 
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     print(f"Found {num_words} total words")
